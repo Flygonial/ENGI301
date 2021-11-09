2caeedbe0be8171f0435052548aab343525c5b01
Samuel Xu <46857705+Flygonial@users.noreply.github.com>
2021-11-08 23:33:10 -0600

    Delete project_01 directory

--- project_01/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# ----------------------------------------------------------------
-# <Same documentation / copyright as above>
-# ----------------------------------------------------------------
-cd /var/lib/cloud9/ENGI301/python/project_01
-#./configure_pins.sh
-sudo python3 air_quality_module.py
